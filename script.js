225ef8a4eade1335eafeef68e94bc47842800ac6
Prakhar812 <Prakhar812000@gmail.com>
2024-08-05 00:55:26 +0530

    page 1 hero section done

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function loadingAnimation(){
   var tl = gsap.timeline();
 tl.from("#line h1, #line h2", {
   y: 150,
-  stagger: 0.2,
-  duration: 0.6,
-  delay: 0.5,
+  // stagger: 0.2,
+  // duration: 0.6,
+  // delay: 0.5,
 });
-tl.from("#line1-part1", {
+tl.from("#countdown", {
   opacity: 0,
   onStart: function () {
-    var h5timer = document.querySelector("#line1-part1 h5");
+    var h5timer = document.querySelector("#countdown h5");
     var grow = 0;
     setInterval(function () {
       if (grow < 100) {
@@ -19,40 +19,53 @@ tl.from("#line1-part1", {
         h5timer.innerHTML = grow;
       }
 
-    }, 35);
+    }, 29);
   },
 });
 
-// tl.to("#line h2",{
-//   animationName: "anime",
-//   opacity:1
-//   })  
 tl.to("#loader", {
   opacity: 0,
-  duration: 0.2,
-  delay: 4,
+  // duration: 0.2,
+  // delay: 3.5,
+  
 });
 tl.from("#page1",{
-  delay:0.2,
+  // delay:0.2,
   y:1600,
   opacity:0,
-  duration:0.5,
-  easePower4
+  // duration:0.5,
+  ease:Power4
 })
 
 tl.to("#loader",{
   display:"none"
 })
+// tl.from("#nav",{
+//   opacity:0
+// })
+tl.from(" #hero h1, #hero3 h2,h3" ,{
+  y:120,
+  stagger:0.2
 
-
-
-
-
-
-
-
-
+})
 
 
 }
 loadingAnimation()
+
+function cursorAnimation(){
+document.addEventListener("mousemove",function(dets){
+  gsap.to("#crsr",{
+
+    left:dets.x,
+    top:dets.y
+  })
+
+})
+
+Shery.makeMagnet("#nav-part2 h4", {
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+})
+}
+cursorAnimation()
