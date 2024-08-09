ada29a08745023a06f90b4cae35710fdce911f17
Prakhar812 <Prakhar812000@gmail.com>
2024-08-10 01:16:27 +0530

    all pages done / locomotive applied

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+function cursorAnimation(){
+  document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+  
+      left:dets.x,
+      top:dets.y
+    })
+  
+  })
+  
+  Shery.makeMagnet("#nav-part2 h4", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  })
+  }
+
+function locomotiveAnimation(){
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+  
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+  });
+}
 //preloader
 function loadingAnimation(){
   var tl = gsap.timeline();
@@ -43,29 +83,35 @@ tl.to("#loader",{
 // tl.from("#nav",{
 //   opacity:0
 // })
-tl.from(" #hero h1, #hero3 h2,h3" ,{
-  y:120,
-  stagger:0.2
+tl.from("#hero1, #hero2, #hero3 , #hero4 " ,{
+  y: 140,
+  stagger: 0.2,
+  // duration: 0.6,
+  // delay: 1,
+  opacity:0,
+  // ease:Power4
 
 })
+
+tl.from("#hero1 ,#page2",{
+  opacity:0,
+},"-=1.2")
+
+// tl.from("#page2",{
+//   // delay:0.2,
+//   y:1200,
+//   opacity:0,
+//   // duration:0.5,
+//   ease:Power4
+// })
 
 
 }
-loadingAnimation()
 
-function cursorAnimation(){
-document.addEventListener("mousemove",function(dets){
-  gsap.to("#crsr",{
 
-    left:dets.x,
-    top:dets.y
-  })
 
-})
+loadingAnimation();
+cursorAnimation();
+locomotiveAnimation();
 
-Shery.makeMagnet("#nav-part2 h4", {
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
-})
-}
-cursorAnimation()
+
